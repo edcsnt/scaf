0560718260fca71da4afb64f9c6c2bf3ba38c032
Eduardo Santos <33993373+edcsnt@users.noreply.github.com>
2025-10-20 12:18:50 -0300

    feat: add Prettier config

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+/* Copyright 2025 edcsnt. All rights reserved. */
+import type { Config } from "prettier";
+
+const config: Config = {
+  experimentalOperatorPosition: "start",
+  printWidth: 72,
+  objectWrap: "collapse",
+  arrowParens: "avoid",
+  proseWrap: "always",
+  singleAttributePerLine: true,
+};
+
+export default config;
